2161d99af8e256cf7fab0383c8ccb373647a3790
Thiago Rodrigues <thiago_thg@outlook.com>
2021-07-03 12:27:28 -0300

    Adicionado dicionário português

---
+++ resources/lang/en/labels.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Labels Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | 
+    | 
+    | 
+    |
+    */
+
+    'home'              => 'Home',
+    'products'          => 'Products',
+    'filter'            => 'Filter',
+    'category'          => 'Category',
+    'product'           => 'Product',
+
+
+    'sent' => 'We have emailed your password reset link!',
+    'throttled' => 'Please wait before retrying.',
+    'token' => 'This password reset token is invalid.',
+    'user' => "We can't find a user with that email address.",
+
+];
---
+++ resources/lang/pt-br/labels.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Labels Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | 
+    | 
+    | 
+    |
+    */
+
+    'home'          => 'Home',
+    'products'      => 'Produtos',
+    'filter'        => 'Filtro',
+    'category'      => 'Categoria',
+    'product'       => 'Produto',
+
+
+    'sent' => 'We have emailed your password reset link!',
+    'throttled' => 'Please wait before retrying.',
+    'token' => 'This password reset token is invalid.',
+    'user' => "We can't find a user with that email address.",
+
+];
