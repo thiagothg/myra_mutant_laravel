5a89161f06d4e85982c7b272d54f6a991b90ca27
Thiago Rodrigues <thiago_thg@outlook.com>
2021-07-04 14:40:25 -0300

    Migrations Categoria & Produto

---
+++ database/migrations/2021_07_03_181434_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tb_cad_product', function (Blueprint $table) {
+            $table->increments('cod_product');
+            $table->string('des_product', 150)->unique();
+            $table->integer('qtd_storage');
+            $table->float('qtd_price');
+            $table->integer('cod_category')->unsigned();
+            $table->foreign('cod_category')->references('cod_category')->on('tb_cad_category');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
